33a657383d3216c811e18d12eccdaac7f56032b5
Mahshad <mahshad.dezhsetan@gmail.com>
2022-09-23 18:44:04 +0400

    Added robot commands txt file aaalso used tags in robot

--- Test/TestCase1.robot
+++ Test/TestCase1.robot
@@ -8,5 +8,14 @@ Documentation   This is my first practice with robotframework!
 
 *** Test Cases ***
 
-TEST_CASE_NAME
-    Log     This is a sample Test Case
+TEST_CASE1
+    [Tags]  TAG1
+    LOG     This is a log for Test Case 1
+
+TEST_CASE2
+    [Tags]  TAG1
+    log     This is a log for Test Case 2
+
+TEST_CASE3
+    [Tags]  TAG2
+    log     This is a log for Test Case 3
--- main.py
+++ main.py
@@ -1,32 +1,10 @@
-import time
-from altunityrunner import AltUnityDriver, By
+from altunityrunner import AltUnityDriver
+
 import setupdriver as setup
-
-
-
 
 appium_driver = setup.setup_driver(port=4723)
 alt_driver = AltUnityDriver()
 print("object created")
-
-
-email_obj.call_component_method("LocalizedText", "SetKey", [email_text])
-print("username added")
-time.sleep(5)
-pass_obj.set_text(pass_text)
-# pass_obj.call_component_method("LocalizedText","SetKey",[pass_text])
-print("password added")
-time.sleep(5)
-signin_btn = alt_driver.find_object(By.ID, "16500")
-signin_btn.click()
-# signin_btn.call_component_method("UnityEngine.UI.Button","Press")
-print("clicked")
-time.sleep(5)
-
-# actions = TouchAction(appium_driver)
-# actions.tap(appium_driver.find_element_by_id("17130"))
-# actions.perform()
-#
 
 
 print("End")
