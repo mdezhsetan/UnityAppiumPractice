0e3222639bf5f80c64e2227657f4fda9d4f4a41b
Mahshad <mahshad.dezhsetan@gmail.com>
2022-10-03 15:58:01 +0400

    parametrize

--- Test/test_example1.py
+++ Test/test_example1.py
@@ -13,3 +13,9 @@ def func(x):
 @pytest.mark.one
 def test_method2(number):
     assert number != 6
+
+
+# We have to use the exact x,y,z parameters as the next method's arguments
+@pytest.mark.parametrize("x,y,z", [(1, 2, 3), (2, 3, 4), (4, 6, 10)])
+def test_param(x, y, z):
+    assert x + y == z
