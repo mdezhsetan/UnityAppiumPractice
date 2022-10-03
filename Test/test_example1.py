0d54293db5dabc39cef489fd3078333cac333205
Mahshad <mahshad.dezhsetan@gmail.com>
2022-10-03 16:14:23 +0400

    pytest parametrize id and used eval method

--- Test/test_example1.py
+++ Test/test_example1.py
@@ -19,3 +19,18 @@ def test_method2(number):
 @pytest.mark.parametrize("x,y,z", [(1, 2, 3), (2, 3, 4), (4, 6, 10)])
 def test_param(x, y, z):
     assert x + y == z
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("3+5", 8),
+        pytest.param("1+7", 8, marks=pytest.mark.basic),
+        pytest.param("2+4", 6, marks=pytest.mark.basic, id="basic_2+4"),
+        pytest.param(
+            "6*9", 42, marks=[pytest.mark.basic, pytest.mark.xfail], id="basic_6*9"
+        ),
+    ],
+)
+def test_param_id(test_input, expected):
+    assert eval(test_input) == expected
